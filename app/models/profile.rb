93cb5d83cf520507beff1da56211d8ab71ea1c8f
Raven Jiang <raven@cs.stanford.edu>
2014-11-29 14:47:49 -0800

    Fixes profile creation.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,7 +3,7 @@ class Profile < ActiveRecord::Base
   validates :first_name, :last_name, :local_street, :local_city, :local_state,
   :local_postal, :perm_street, :perm_city, :perm_state, :perm_country,
   :perm_postal, :majors, :class_year, :overall_gpa, :major_gpa, presence: true
-  after_save :update_user_name
+  after_commit :update_user_name
 
   def full_name
     if middle_name.blank?
