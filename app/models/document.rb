84921b516872f59da2c46fa051d39a143fa8b03b
Raven Jiang <raven@cs.stanford.edu>
2014-11-29 15:27:07 -0800

    Adds 1 MB file size limit for uploading PDFs.

--- app/models/document.rb
+++ app/models/document.rb
@@ -6,4 +6,5 @@ class Document < ActiveRecord::Base
   has_attached_file :file
   validates_attachment_content_type :file, content_type: ['application/pdf']
   validates_attachment_presence :file
+  validates_attachment_size :file, less_than: 1.megabyte
 end
