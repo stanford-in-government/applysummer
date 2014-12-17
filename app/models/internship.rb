8ba68eb955f3ce0c7539b6edd120a275c038be9b
Raven Jiang <raven@cs.stanford.edu>
2014-12-17 09:37:29 +0800

    Minor stipend tweaks.

--- app/models/internship.rb
+++ app/models/internship.rb
@@ -22,7 +22,7 @@ class Internship < ActiveRecord::Base
   SHORT_ANSWER_QUESTIONS = {
     work_scope: 'Describe the specific work scope and tasks in your 9-week internship. What is the role of the office in policymaking? How will your internship relate to public policy (government functions related to policy-making, research related to public policy or government, implementation or development of policy)? (200 words max)',
     # relevance: 'Describe how your internship is, by nature, in public policy, as defined above (government functions related to policy-making, research related to public policy or government, implementation or development of policy). (100 words max)',
-    reason: 'Elaborate on your reason for applying for a stipend. Would you be able to take this unpaid internship without the stipend? What are your plans in case you do not receive a stipend?',
+    reason: 'What financial considerations would you like us to know about? What are your plans in case you do not receive a stipend? (100 words max)'
   }
 
   CATEGORIES = [
